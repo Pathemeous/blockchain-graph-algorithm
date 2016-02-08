61c7d951221d0561915cf95a310bd977e5e8dd61
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2016-02-08 15:45:11 +0100

    Fix multiple edges between the same peers (SMALL_WORLD, UNIFORM)

--- src/main/java/nl/tudelft/ti2306/blockchain/PeerGraphGenerator.java
+++ src/main/java/nl/tudelft/ti2306/blockchain/PeerGraphGenerator.java
@@ -13,7 +13,7 @@ public class PeerGraphGenerator {
     public static final int SMALL_WORLD = 0;
     public static final int SCALE_FREE = 1;
     public static final int UNIFORM = 2;
-    
+
     private static Random random = new Random();
 
     /**
@@ -30,15 +30,17 @@ public class PeerGraphGenerator {
     public static PeerGraph generate(int peerCount, int method, double degree, double... params) {
         if (peerCount == 1)
             return new PeerGraph(1);
-        
+
         PeerGraph res = new PeerGraph(peerCount);
         switch (method) {
         case SMALL_WORLD: 
             for (int peer1 = 0; peer1 < peerCount; peer1++) {
-                for (int i = 1; i < degree / 2 + 1; i++) {
+                for (int i = 1; i <= degree / 2; i++) {
                     if (random.nextDouble() < params[0]) {
                         int peer2;
-                        while ((peer2 = random.nextInt(peerCount)) == peer1)
+                        while ((peer2 = random.nextInt(peerCount)) == peer1
+                                || res.getEdges(peer1).contains(peer2)
+                                || inRange(peer1 - degree/2, peer2, peer1 + degree/2, peerCount))
                             continue;
                         res.addEdge(peer1, peer2);
                     } else {
@@ -71,13 +73,22 @@ public class PeerGraphGenerator {
             for (int edges = 0; edges < peerCount * degree / 2; edges++) {
                 int peer1 = random.nextInt(peerCount);
                 int peer2;
-                while ((peer2 = random.nextInt(peerCount)) == peer1)
+                while ((peer2 = random.nextInt(peerCount)) == peer1
+                        || res.getEdges(peer1).contains(peer2))
                     continue;
                 res.addEdge(peer1, peer2);
             }
             break;
         }
         return res;
+    }
+
+    private static boolean inRange(double bound1, int value, double bound2,
+            int range) {
+        // TODO something with modulo probably looks better
+        return (bound1 <= value && value <= bound2
+                || bound1 - range <= value && value <= bound2 - range
+                || bound1 + range <= value && value <= bound2 + range);
     }
 
 }
